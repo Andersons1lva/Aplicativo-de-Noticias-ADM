584122aae3daf512eaffb129fdd36e56ae4a3a76
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-17 18:13:14 -0300

    Create fun salvarNoticia() and limparCampos().

--- app/src/main/java/com/anderson/appdenoticiasadm/MainActivity.kt
+++ app/src/main/java/com/anderson/appdenoticiasadm/MainActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.anderson.appdenoticiasadm.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +27,41 @@ class MainActivity : AppCompatActivity() {
             val data = binding.editData.text.toString()
             val autor = binding.editAutor.text.toString()
 
+            //validação dos campos, se estão preenchidos ou não.
             if (titulo.isEmpty() || noticia.isEmpty() || data.isEmpty() || autor.isEmpty()){
                 Toast.makeText(this,"Preencha todos os campos.", Toast.LENGTH_SHORT).show()
+            }else{
+                salvarNoticia(titulo,noticia,data,autor)
             }
+
         }
+    }
+    // salvar as informação no banco de dados.
+    private fun salvarNoticia(titulo: String, noticia: String, data: String, autor: String){
+        // criação de chaves para cada campo.
+        val mapNoticias = hashMapOf(
+            "titulo" to titulo,
+            "noticia" to noticia,
+            "data" to data,
+            "autor" to autor,
+        )
+
+        // interação junto ao banco de dados.
+        db.collection("noticias").document("noticia")
+            .set(mapNoticias).addOnCompleteListener { tarefa ->
+                if (tarefa.isSuccessful){
+                    Toast.makeText(this,"Notícia publicada com sucesso!", Toast.LENGTH_SHORT).show()
+                    limparCampos()
+                }
+            }.addOnFailureListener {
+
+            }
+    }
+    private fun limparCampos(){
+        // reset nos campos
+        binding.editTitleNoticia.setText("")
+        binding.editNoticia.setText("")
+        binding.editData.setText("")
+        binding.editAutor.setText("")
     }
 }
