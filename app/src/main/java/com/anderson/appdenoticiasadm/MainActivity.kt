14e9c9c643c8130fd9cb8faf8aac8031d06211ef
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-16 23:24:37 -0300

    Create Event click button save.

--- app/src/main/java/com/anderson/appdenoticiasadm/MainActivity.kt
+++ app/src/main/java/com/anderson/appdenoticiasadm/MainActivity.kt
@@ -2,15 +2,31 @@ package com.anderson.appdenoticiasadm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.anderson.appdenoticiasadm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // para ocultar a toolbar de ação
         supportActionBar!!.hide()
+
+        //evento de click
+        binding.btnPublica.setOnClickListener {
+            val titulo = binding.editTitleNoticia.text.toString()
+            val noticia = binding.editNoticia.text.toString()
+            val data = binding.editData.text.toString()
+            val autor = binding.editAutor.text.toString()
+
+            if (titulo.isEmpty() || noticia.isEmpty() || data.isEmpty() || autor.isEmpty()){
+                Toast.makeText(this,"Preencha todos os campos.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
